c4f40afb8fd67d5cfc085d574142de817ed45571
Barlas Söker <bsoker98@gmail.com>
2025-07-05 15:53:45 +0200

    Added data RAM for a Harvard architecture
    1-cycle latency of the BRAM should be handled somehow

---
+++ src/ram.vhd
@@ -0,0 +1,44 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity ram is
+    generic (
+        addr_width : integer := 10;  -- 2^10 = 1024 words = 4KB
+        data_width : integer := 32
+    );
+    port (
+        clk       : in  std_logic;
+        addr      : in  std_logic_vector(addr_width - 1 downto 0);
+        write_en  : in  std_logic;
+        write_data: in  std_logic_vector(data_width - 1 downto 0);
+        read_data : out std_logic_vector(data_width - 1 downto 0)
+    );
+end ram;
+
+architecture Behavioral of ram is
+    type ram_type is array(0 to 2**addr_width - 1) of std_logic_vector(7 downto 0);
+    signal mem : ram_type := (others => (others => '0'));
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if write_en = '1' then
+                -- write 4 bytes at once to consecutive addresses
+                mem(to_integer(unsigned(addr)))     <= write_data(7 downto 0);
+                mem(to_integer(unsigned(addr)) + 1) <= write_data(15 downto 8);
+                mem(to_integer(unsigned(addr)) + 2) <= write_data(23 downto 16);
+                mem(to_integer(unsigned(addr)) + 3) <= write_data(31 downto 24);
+            end if;
+        end if;
+    end process;
+
+    -- Asynchronous read
+    read_data <= 
+        mem(to_integer(unsigned(addr)))     &
+        mem(to_integer(unsigned(addr)) + 1) &
+        mem(to_integer(unsigned(addr)) + 2) &
+        mem(to_integer(unsigned(addr)) + 3);
+
+end Behavioral;
+
--- src/rom.vhd
+++ src/rom.vhd
@@ -4,19 +4,19 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity rom is
     generic (
-        addr_width  : integer := 10;  -- 2^10 = 1024 bytes
-        mem_size    : integer := 1024 -- number of 1-byte entries
+        addr_width  : integer := 10;  -- 2^10 = 1024 words
+        data_width  : integer := 32 
     );
     port (
-        addr : in  std_logic_vector(addr_width-1 downto 0); -- Byte address (PC)
-        data : out std_logic_vector(31 downto 0)            -- Full 32-bit instruction
+        addr : in  std_logic_vector(addr_width - 1 downto 0); -- Byte address (PC)
+        data : out std_logic_vector(data_width - 1 downto 0)            -- Full 32-bit instruction
     );
 end rom;
 
 architecture rom_arch of rom is
 
     -- Byte-addressable ROM: Each address holds 1 byte
-    type rom_type is array (0 to mem_size-1) of std_logic_vector(7 downto 0);
+    type rom_type is array (0 to 2**addr_width - 1) of std_logic_vector(7 downto 0);
     signal rom_array : rom_type := (
         -- Example:
         0  => x"67",  -- byte 0 of instr 0
