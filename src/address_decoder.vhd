98afdbba16e25de987ceba0e37dcd032442d0a40
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2025-07-19 01:02:04 +0200

    Added a MMIO UART module to test the overall CPU.
    Communication between the CPU and UART is a success and the Letter
    'H' can be seen on a UART program like screen or minicom!

--- src/address_decoder.vhd
+++ src/address_decoder.vhd
@@ -7,7 +7,8 @@ entity address_decoder is
         addr         : in  std_logic_vector(31 downto 0);  -- alu_result
         ram_en       : out std_logic;
         ram_addr     : out std_logic_vector(11 downto 0);  -- 4KB RAM
-        uart_en      : out std_logic
+        uart_en      : out std_logic;
+        uart_addr    : out std_logic_vector(1 downto 0)     -- select data/status
     );
 end address_decoder;
 
@@ -15,11 +16,12 @@ architecture Behavioral of address_decoder is
 begin
 
     -- RAM: 0x10000000 – 0x10000FFF
-    ram_en <= '1' when addr(31 downto 12) = x"10000" else '0';
-    ram_addr <= addr(11 downto 0); -- translate global addr to local RAM offset
+    ram_en   <= '1' when addr(31 downto 12) = x"10000" else '0';
+    ram_addr <= addr(11 downto 0);  -- word-aligned inside RAM
 
-    -- UART: 0x20000000 (example address)
-    uart_en <= '1' when addr(31 downto 12) = x"20000" else '0';
+    -- UART: 0x20000000 and 0x20000004 
+    uart_en   <= '1' when addr(31 downto 12) = x"20000" else '0';
+    uart_addr <= addr(3 downto 2);  -- extract UART register address (00 = data, 01 = status)
 
 end Behavioral;
 
