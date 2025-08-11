3702ae40714b477604b5a7c285745c9d54bd8d5e
Barlas Söker <bsoker98@gmail.com>
2025-08-11 14:46:39 +0200

    Patch build.sh

--- build.sh
+++ build.sh
@@ -8,25 +8,25 @@ BIN=build/main.bin
 DUMP=build/dump.txt
 
 # === Compilation ===
-echo "[1/6] Compiling main.c and start.s..."
+echo "[1/5] Compiling main.c and start.s..."
 riscv-none-elf-gcc -march=rv32i -mabi=ilp32 \
   -T toolchain/linker.ld -nostartfiles -ffreestanding \
   -o "$ELF" toolchain/start.s toolchain/main.c
 
 # === ELF to Binary ===
-echo "[2/6] Generating binary..."
+echo "[2/5] Generating binary..."
 riscv-none-elf-objcopy -O binary "$ELF" "$BIN"
 
 # === Dump disassembly ===
-echo "[3/6] Generating dump..."
+echo "[3/5] Generating dump..."
 riscv-none-elf-objdump -d "$ELF" > "$DUMP"
 
 # === Convert binary to hex/byte rows for ROM init ===
-echo "[4/6] Converting binary..."
+echo "[4/5] Converting binary..."
 python3 toolchain/convert.py
 
 # === Fill ROM with padded values or formatted lines ===
-echo "[5/6] Filling ROM..."
+echo "[5/5] Filling ROM..."
 python3 toolchain/prepare_rom.py
 python3 toolchain/fill_rom.py
 
